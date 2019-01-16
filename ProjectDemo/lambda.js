c6be342df41f1aa2d14abf1a630c8bcc6b4ad36a
DillonLakshman <dillonlakshman@gmail.com>
2019-01-16 12:39:30 +0530

    commit 4

--- ProjectDemo/lambda.js
+++ ProjectDemo/lambda.js
@@ -1,20 +1,38 @@
 let Swagger = require('swagger-client');
 
 exports.handler = function (event, context, callback) {
-    Swagger.http({
-        url: `https://apac.faceid.hyperverge.co/v1/photo/verifyPair`,
-        method: 'post',
-        query: {},
-        headers: {
+    
+    var http = require("https");
+
+    var options = {
+        "method": "POST",
+        "hostname": "ind.faceid.hyperverge.co",
+        "port": null,
+        "path": "/v1/photo/verifyPair",
+        "headers": {
+            "content-type": "multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW",
             "appid": "2d9288",
             "appkey": "506505f70970ce16988f",
-        },
-        body: ''
-    }).then((response) => {
-        console.log(response)
-    }).catch((err) => {
-        console.log(err)
+            "cache-control": "no-cache",
+            "postman-token": "8f2ec2f7-04f0-c64d-cfd8-263e3648ecb0"
+        }
+    };
+
+    var req = http.request(options, function (res) {
+        var chunks = [];
+
+        res.on("data", function (chunk) {
+            chunks.push(chunk);
+        });
+
+        res.on("end", function () {
+            var body = Buffer.concat(chunks);
+            console.log(body.toString());
+        });
     });
+
+    req.write("------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"type\"\r\n\r\nid\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image1\"; filename=\"vasu2.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW\r\nContent-Disposition: form-data; name=\"image2\"; filename=\"VasuLinkedIn.jpg\"\r\nContent-Type: image/jpeg\r\n\r\n\r\n------WebKitFormBoundary7MA4YWxkTrZu0gW--");
+    req.end();
 
     callback(null, { "message": "Successfully executed" });
 }
