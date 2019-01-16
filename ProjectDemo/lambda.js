10c8f808d8d55e6f2fa9d2d8430d077765b587ef
DillonLakshman <dillonlakshman@gmail.com>
2019-01-16 12:46:30 +0530

    commit 6

--- ProjectDemo/lambda.js
+++ ProjectDemo/lambda.js
@@ -1,8 +1,7 @@
 let Swagger = require('swagger-client');
+let request = require('request').defaults({ encoding: null });
 
-exports.handler = function (event, context, callback) {
-
-    var request = require('request').defaults({ encoding: null });
+exports.handler = function (event, context, callback) {    
 
     request.get('https://s3-ap-southeast-1.amazonaws.com/upload-to-s3-sample/VasuLinkedIn.jpg', function (error, response, body) {
         if (!error && response.statusCode == 200) {
