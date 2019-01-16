fb96b2d71b8a1965b22db46c6881a7f2eaf87c0e
DillonLakshman <dillonlakshman@gmail.com>
2019-01-16 17:27:33 +0530

    init

--- ProjectDemo/lambda.js
+++ ProjectDemo/lambda.js
@@ -1,14 +1,5 @@
-let Swagger = require('swagger-client');
-let request = require('request').defaults({ encoding: null });
+exports.handler = function(event, context, callback) {   
+    
 
-exports.handler = function (event, context, callback) {    
-
-    request.get('https://s3-ap-southeast-1.amazonaws.com/upload-to-s3-sample/VasuLinkedIn.jpg', function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-            data = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
-            console.log(data);
-        }
-    });
-
-    callback(null, { "message": "Successfully executed" });
+    callback(null, {"message": "Successfully executed"});
 }
