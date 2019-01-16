dc321fa8a9043b50bdb33eb7643ba21c486c5b3d
DillonLakshman <dillonlakshman@gmail.com>
2019-01-16 18:39:20 +0530

    commit main 4

--- ProjectDemo/lambda.js
+++ ProjectDemo/lambda.js
@@ -15,21 +15,22 @@ exports.handler = function (event, context, callback) {
         body: '{"password": "1qaz2wsx@","userName": "rwijetilleke@virtusa.com"}'
     }).then((response) => {
         bearer = response.body.access_token
+
+            Swagger.http({
+            url: `https://api.apixplatform.com/facematch/1.0/v1/photo/verifyPair`,
+            method: 'post',
+            query: {},
+            headers: { "appId": "2d9288", "appKey": "506505f70970ce16988f", "X-Authorization": bearer},
+            body: `image1=${event.image1}&image2=${event.image2}&type=id`
+        }).then((response) => {
+            console.log(response)
+        }).catch((err) => {
+            console.log(err)
+        });
+
     }).catch((err) => {
         console.log("here 2")
-    });
-
-    Swagger.http({
-        url: `https://api.apixplatform.com/facematch/1.0/v1/photo/verifyPair`,
-        method: 'post',
-        query: {},
-        headers: { "appId": "2d9288", "appKey": "506505f70970ce16988f", "X-Authorization": bearer},
-        body: `image1=${event.image1}&image2=${event.image2}&type=id`
-    }).then((response) => {
-        confirm.log(response)
-    }).catch((err) => {
-        console.log(err)
-    });
+    });    
 
     callback(null, event.type);
 }
