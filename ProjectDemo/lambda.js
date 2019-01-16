0e1668d381fc4aeb907dda3448664cc1597f959b
DillonLakshman <dillonlakshman@gmail.com>
2019-01-16 18:14:07 +0530

    commit main

--- ProjectDemo/lambda.js
+++ ProjectDemo/lambda.js
@@ -2,15 +2,16 @@ let Swagger = require('swagger-client');
 
 exports.handler = function (event, context, callback) {
 
+    console.log(event.image1);
+    console.log(event.image2);
+    console.log(event.type);
+
     Swagger.http({
         url: `https://services.apixplatform.com/api-sandbox/application/token`,
         method: 'post',
         query: {},
         headers: { "Accept": "*/*", "Content-Type": "application/json" },
-        body: {
-            "password": "1qaz2wsx@",
-            "userName": "rwijetilleke@virtusa.com"
-        }
+        body: '{"password": "1qaz2wsx@","userName": "rwijetilleke@virtusa.com"}'
     }).then((response) => {
         console.log("here");
     }).catch((err) => {
