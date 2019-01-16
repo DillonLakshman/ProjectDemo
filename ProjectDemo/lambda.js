d5285b3da3acb17299272684220eefec13c79a04
DillonLakshman <dillonlakshman@gmail.com>
2019-01-16 18:46:31 +0530

    commit main 6

--- ProjectDemo/lambda.js
+++ ProjectDemo/lambda.js
@@ -1,5 +1,5 @@
 let Swagger = require('swagger-client');
-let bearer = "undefined";
+let result = "undefined";
 
 exports.handler = function (event, context, callback) {
 
@@ -8,29 +8,16 @@ exports.handler = function (event, context, callback) {
     console.log(event.type);
 
     Swagger.http({
-        url: `https://services.apixplatform.com/api-sandbox/application/token`,
+        url: `https://apac.faceid.hyperverge.co/v1/photo/verifyPair`,
         method: 'post',
         query: {},
-        headers: { "Accept": "*/*", "Content-Type": "application/json" },
-        body: '{"password": "1qaz2wsx@","userName": "rwijetilleke@virtusa.com"}'
-    }).then((response) => {
-        bearer = response.body.access_token
+        headers: { "appId": "2d9288", "appKey": "506505f70970ce16988f"},
+        body: `image1=${event.image1}&image2=${event.image2}&type=id`
+            }).then((response) => {
+                result = response
+            }).catch((err) => {
+                console.log(err)
+            });   
 
-            Swagger.http({
-            url: `https://apac.faceid.hyperverge.co/v1/photo/verifyPair`,
-            method: 'post',
-            query: {},
-            headers: { "appId": "2d9288", "appKey": "506505f70970ce16988f"},
-            body: `image1=${event.image1}&image2=${event.image2}&type=id`
-        }).then((response) => {
-            console.log(response)
-        }).catch((err) => {
-            console.log(err)
-        });
-
-    }).catch((err) => {
-        console.log("here 2")
-    });    
-
-    callback(null, event.type);
+    callback(null, result);
 }
