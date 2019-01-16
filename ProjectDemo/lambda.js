74c7c2eac66c72fee5fdce706387b461bb151cac
DillonLakshman <dillonlakshman@gmail.com>
2019-01-16 12:34:58 +0530

    commit 1

---
+++ ProjectDemo/lambda.js
@@ -0,0 +1,20 @@
+let Swagger = require('swagger-client');
+
+exports.handler = function (event, context, callback) {
+    Swagger.http({
+        url: `https://apac.faceid.hyperverge.co/v1/photo/verifyPair`,
+        method: 'post',
+        query: {},
+        headers: {
+            "appid": "2d9288",
+            "appkey": "506505f70970ce16988f",
+        },
+        body: 'image1=vasu.jpg&image2=vasu.jpg&type=id'
+    }).then((response) => {
+        console.log(response)
+    }).catch((err) => {
+        console.log(err)
+    });
+
+    callback(null, { "message": "Successfully executed" });
+}
