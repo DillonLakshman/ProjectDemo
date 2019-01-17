d145e7c1f5db068ba1a0bbafd05b674a5cc69ec8
DillonLakshman <dillonlakshman@gmail.com>
2019-01-17 11:49:22 +0530

    face match try

--- ProjectDemo/lambda.js
+++ ProjectDemo/lambda.js
@@ -1,5 +1,6 @@
 let Swagger = require('swagger-client');
 let fs = require('fs');
+let request = require("request");
 
 
 exports.handler = function (event, context, callback) {
@@ -7,9 +8,26 @@ exports.handler = function (event, context, callback) {
     let image1 = event.image1;
 
     let buff = new Buffer(image1, 'base64');
-    fs.writeFileSync('image1.jpg', buff);
+    fs.writeFileSync('/tmp/image1.jpg', buff);
 
-    callback(null, 'Base64 image data converted to file: stack-abuse-logo-out.png');
+    let image2 = event.image2;
+
+    let buff2 = new Buffer(image2, 'base64');
+    fs.writeFileSync('/tmp/image2.jpg', buff2);
+
+
+    Swagger.http({
+        url: `https://apac.faceid.hyperverge.co/v1/photo/verifyPair`,
+        method: 'post',
+        query: {},
+        headers: { "appId": "2d9288", "appKey": "506505f70970ce16988f"},
+        body: `image1=%2Ftmp%2Fimage1.jpg&image2=%2Ftmp%2Fimage2.jpg&type=id`
+    }).then((response) => {
+        callback(null, response.body);
+    }).catch((err) => {
+        console.log(err);
+    });
+
 
     if (event.method == "TOKEN") {
         Swagger.http({
